536f219fc56b72716fb77bbdb9dc3c15250b8411
mpetrasin <milos.petrasin@gmail.com>
2022-12-15 22:35:05 +0100

    Add Configuration Properties

--- src/test/java/com/polarbookshop/catalogservice/HomeControllerTest.java
+++ src/test/java/com/polarbookshop/catalogservice/HomeControllerTest.java
@@ -1,4 +1,4 @@
-package com.polarbookshop.catalogservice.controller;
+package com.polarbookshop.catalogservice;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
